f0cb51ac6e87a2224519f38ca8c03870a43de136
stonker.lee <stonker.lee@gmail.com>
2011-03-17 01:42:26 +0000

    1. Remove static import

--- src/org/ray/upnp/ssdp/SSDPRespMsg.java
+++ src/org/ray/upnp/ssdp/SSDPRespMsg.java
@@ -1,13 +1,10 @@
 package org.ray.upnp.ssdp;
 
 import java.net.DatagramPacket;
-import java.util.Scanner;
 
 public class SSDPRespMsg {
     public static boolean isSSDPRespMsg(DatagramPacket dp) {
-        String content = new String(dp.getData());
-        Scanner s = new Scanner(content);
-        String startLine = s.nextLine();
+        String startLine = SSDP.parseStartLine(dp);
         if (SSDP.SL_OK.equals(startLine)) {
             return true;
         }
